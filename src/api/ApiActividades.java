692cf8727ad5810f496c1a55b7058837cef7c65f
Galder <Galder@DESKTOP-ALL3S8I>
2020-05-28 10:10:01 +0200

    ApiActividades eginda

--- src/api/ApiActividades.java
+++ src/api/ApiActividades.java
@@ -3,6 +3,7 @@ package api;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -23,36 +24,47 @@ import modelo.dao.ModeloActividad;
 @WebServlet("/ApiActividades")
 public class ApiActividades extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ApiActividades() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ModeloActividad modeloActividad = new ModeloActividad();
-		ArrayList<Actividad> actividades = modeloActividad.selectAll();
-		
-		response.setHeader("Access-Control-Allow-Origin", "*");
-		response.setContentType("application/json");
-		
-		String jsonString = JSONStringer.valueToString(actividades);
-		
-		PrintWriter out = response.getWriter();
-		out.print(jsonString);
-		out.flush();
+	public ApiActividades() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		ModeloActividad modeloActividad = new ModeloActividad();
+		ArrayList<Actividad> actividades = modeloActividad.selectAll();
+
+		String jsonString = JSONStringer.valueToString(actividades);
+
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(response.getOutputStream(), "UTF8"), true);
+
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+
+		out.print(jsonString);
+		out.flush();
+		out.close();
+
+		try {
+			modeloActividad.getConexion().close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
